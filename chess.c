8fa35670837334c0bc539d5007d6ec5fa84400bc
ahtml <anandhtml@gmail.com>
2018-01-02 18:23:17 -0500

    Used the pieces in the board array and modified print formatting.

--- chess.c
+++ chess.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
-char mainBoard [8][8];
+
 struct ChessPiece
 {
 	char pieceName;
@@ -10,54 +10,67 @@ struct ChessPiece
 	B: Bishop (3 points)
 	P: Pawn (1 point)
 	Q: Queen (9 points)
+	-: No piece
 	*/
-	char colour; // W for white and B for black
 	bool validPos[8][8]; // Where the piece can move
 };
+
+struct ChessPiece mainBoard [8][8];
 
 // Function to initialize all the pieces
 void initializeBoard(){
 	// Pieces for black side
-	mainBoard[0][0] = 'R';
-	mainBoard[0][1] = 'N';
-	mainBoard[0][2] = 'B';
-	mainBoard[0][3] = 'Q';
-	mainBoard[0][4] = 'K';
-	mainBoard[0][5] = 'B';
-	mainBoard[0][6] = 'N';
-	mainBoard[0][7] = 'R';
+	mainBoard[0][0].pieceName = 'R';
+	mainBoard[0][1].pieceName = 'N';
+	mainBoard[0][2].pieceName = 'B';
+	mainBoard[0][3].pieceName = 'Q';
+	mainBoard[0][4].pieceName = 'K';
+	mainBoard[0][5].pieceName = 'B';
+	mainBoard[0][6].pieceName = 'N';
+	mainBoard[0][7].pieceName = 'R';
 	// Setting up pawns for black side
 	for(int i = 0; i<8;i++){
-		mainBoard[1][i] = 'P';
+		mainBoard[1][i].pieceName = 'P';
 	}
 	// Setting up initial blank pieces
 	for(int i = 2; i<6;i++){
 		for(int j=0;j<8;j++){
-			mainBoard[i][j] = '-';
+			mainBoard[i][j].pieceName = '-';
 		}
 	}
 	// Pieces for white side
 	for(int i = 0; i<8;i++){
-		mainBoard[6][i] = 'p';
+		mainBoard[6][i].pieceName = 'p';
 	}
-	mainBoard[7][0] = 'r';
-	mainBoard[7][1] = 'n';
-	mainBoard[7][2] = 'b';
-	mainBoard[7][3] = 'q';
-	mainBoard[7][4] = 'k';
-	mainBoard[7][5] = 'b';
-	mainBoard[7][6] = 'n';
-	mainBoard[7][7] = 'r';
+	mainBoard[7][0].pieceName = 'r';
+	mainBoard[7][1].pieceName = 'n';
+	mainBoard[7][2].pieceName = 'b';
+	mainBoard[7][3].pieceName = 'q';
+	mainBoard[7][4].pieceName = 'k';
+	mainBoard[7][5].pieceName = 'b';
+	mainBoard[7][6].pieceName = 'n';
+	mainBoard[7][7].pieceName = 'r';
 }
 
 // Printing the board
 void printBoard(){
-	for(int i = 0;i<8;i++){
-		for(int j = 0;j<8;j++){
-			printf("%c ",mainBoard[i][j]);
-		}
-		printf("\n");
+	printf(" |");
+	for(int i=1;i<8;i++){
+		printf("%d ",i);
 	}
+	printf("8|\n");
+	for(int i = 0;i<8;i++){
+		printf("%c|",65+i);
+		for(int j = 0;j<7;j++){
+			printf("%c ",mainBoard[i][j].pieceName);
+		}
+		printf("%c|%c\n",mainBoard[i][7].pieceName,65+i);
+	}
+	printf(" |");
+	for(int i=1;i<8;i++){
+		printf("%d ",i);
+	}
+	printf("8|\n");
 }
 
 int main(){
